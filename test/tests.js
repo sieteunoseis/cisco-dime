208e1d5acb2d605a0192859be9c36517cc7f898d
Jeremy <jeremy.worden@gmail.com>
2024-07-15 12:59:23 -0700

    v1.8.3

--- test/tests.js
+++ test/tests.js
@@ -5,6 +5,16 @@ const fse = require("fs-extra");
 const { cleanEnv, str, host } = require("envalid");
 const path = require("path");
 
+// Set up vanilla date time variables
+let date_ob = new Date();
+let date_ob_5 = new Date(date_ob);
+date_ob_5.setMinutes(date_ob.getMinutes() - 160);
+let currentCalendar = date_ob.toLocaleString().split(',')[0]
+let currentTime = date_ob.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+let futureTime = date_ob_5.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+let currentDateTime = currentCalendar.concat(" ", currentTime)
+let pastDateTime = currentCalendar.concat(" ",futureTime)
+
 // If not production load the local env file
 if (process.env.NODE_ENV === "development") {
   require("dotenv").config({
@@ -34,13 +44,13 @@ var servers = [
   {
     hostname: env.CUCM_HOSTNAME,
     username: env.CUCM_USERNAME,
-    password: env.CUCM_USERNAME,
+    password: env.CUCM_PASSWORD,
     filename: "/var/log/active/platform/cli/testcapture.cap01",
   },
   {
     hostname: env.CUCM_HOSTNAME,
     username: env.CUCM_USERNAME,
-    password: env.CUCM_USERNAME,
+    password: env.CUCM_PASSWORD,
     filename: "/var/log/active/platform/cli/testcapture.cap02",
   },
 ];
@@ -64,62 +74,64 @@ if (servers) {
     console.log(serviceLogsNames);
   })();
 
-  // (async () => {
-  //   await blueBirdPromise
-  //     .map(servers, async function (server) {
-  //       let output = await ciscoSoap
-  //         .listNodeServiceLogs(
-  //           server.hostname,
-  //           server.username,
-  //           server.password
-  //         )
-  //         .catch((err) => {
-  //           console.log(err);
-  //           return false;
-  //         });
-  //       return output;
-  //     })
-  //     .then(function (results) {
-  //       console.log(
-  //         "The listNodeServiceLogs method returns the node names in the cluster and the lists of associated service names."
-  //       );
-  //       console.log(results);
-  //     });
-  // })();
+  (async () => {
+    await blueBirdPromise
+      .map(servers, async function (server) {
+        let output = await ciscoDime
+          .listNodeServiceLogs(
+            server.hostname,
+            server.username,
+            server.password
+          )
+          .catch((err) => {
+            console.log(err);
+            return false;
+          });
+        return output;
+      })
+      .then(function (results) {
+        console.log(
+          "The listNodeServiceLogs method returns the node names in the cluster and the lists of associated service names."
+        );
+        console.log(results);
+      });
+  })();
 
-  // (async () => {
-  //   // host,username,password,servicelog,fromdate,todate,timezone
-  //   await blueBirdPromise
-  //     .map(servers, async function (server) {
-  //       let output = await ciscoSoap
-  //         .selectLogFiles(
-  //           server.hostname,
-  //           server.username,
-  //           server.password,
-  //           "Packet Capture Logs",
-  //           "10/09/2022 11:50 AM", // From Date
-  //           "10/09/2022 02:52 PM", // To Date
-  //           "Client: (GMT-8:0)America/Los_Angeles" // Client: (GMT+0:0)Greenwich Mean Time-Europe/London
-  //         )
-  //         .catch((err) => {
-  //           console.log(err, server.hostname);
-  //           return false;
-  //         });
-  //       return output;
-  //     })
-  //     .then(function (results) {
-  //       console.log(
-  //         "The selectLogFiles method lists available service log files, or requests 'push' delivery of service log files based on a selection criteria."
-  //       );
-  //       var flattened = [].concat.apply([],results); // Flatten results
-  //       console.log(flattened);
-  //     });
-  // })();
+  (async () => {
+    // host,username,password,servicelog,fromdate,todate,timezone
+    await blueBirdPromise
+      .map(servers, async function (server) {
+        let output = await ciscoDime
+          .selectLogFiles(
+            server.hostname,
+            server.username,
+            server.password,
+            "Cisco CallManager",
+            pastDateTime, // From Date
+            currentDateTime, // To Date
+            "Client: (GMT-8:0)America/Los_Angeles" // Client: (GMT+0:0)Greenwich Mean Time-Europe/London
+          )
+          .catch((err) => {
+            console.log(err, server.hostname);
+            return false;
+          });
+        return output;
+      })
+      .then(function (results) {
+        console.log(
+          "The selectLogFiles method lists available service log files, or requests 'push' delivery of service log files based on a selection criteria."
+        );
+        var flattened = [].concat.apply([],results); // Flatten results
+        console.log(flattened);
+      });
+  })();
+
+  // TODO: Add test to download files
 
   // (async () => {
   //   await blueBirdPromise
   //     .map(servers, function (server) {
-  //       return ciscoSoap
+  //       return ciscoDime
   //         .getOneFile(
   //           server.hostname,
   //           server.username,
